a624a9a90aa470338d10d5051818746a55b302f8
Wung Jae Lee <wunglee2021@u.northwestern.edu>
2019-12-20 16:23:49 -0600

    constants file for spending/income categories, in need of constant modification, maybe write a program that updates constant file?

---
+++ constants.rkt
@@ -0,0 +1,37 @@
+(module constants racket
+  (provide (all-defined-out))
+
+  (define LEISURE
+    (list "NARA"
+          "SOUND-BAR"
+          "YEOWOOSAI"
+          "CLASSIC LOUNGE"))
+
+  (define TRANSPORTATION
+    (list "UBER" "LYFT"))
+
+  (define ACADEMICS
+    (list "UDEMY"))
+
+  (define FOOD-DRINK
+    (list "POKE"
+          "WHOLEFDS"
+          "TRADER JOE'S"
+          "KUNG FU TEA"
+          "STARBUCKS"
+          "H MART"
+          "CUPITOL"
+          "POTBELLY"
+          "UBER   EATS"
+          "KOCO TABLE"
+          "TEALICIOUS BUBBLE"
+          "SEA RANCH"
+          "OLIVE MEDIT"
+          "FIG & OLIVE"
+          "MUMBAI"
+          "MAGNOLIA"
+          "NU PATTY2"
+          "PRO SAMGYUBSAL"
+          "TODOROKI"
+          "SHAKE SHACK"
+          "The Gage")))
