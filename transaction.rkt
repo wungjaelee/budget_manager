4f0368b52ba2b30f073c21c6fc90e5cbf4a315cf
Wung Jae Lee <wunglee2021@u.northwestern.edu>
2019-12-20 18:51:24 -0600

    monthly-report, preprocessing transaction file

--- transaction.rkt
+++ transaction.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(require "constants.rkt")
+(require "utils.rkt")
 
 ;; contracts
 (define (transaction? t)
@@ -36,41 +38,108 @@
                                   (rest pats))]))
 
 ;; remove unnecessary information in Name
-(define (extract-info name)
+(define (remove-unnecessary-info name)
   (remove-all-pattern name
                       '("DEBIT PURCHASE " "-VISA ")))
 
 (define (line->transaction line)
-  (list-update 
-   (list-update (string-split (remove-doublequote line) ",")
-                4
-                string->number)
-   2
-   extract-info))
+  (compose (list
+            (lambda (line) (string-split (remove-doublequote line) ","))
+            (lambda (lst) (remove-nths lst 1 3)) ;removing transaction-type, memo column
+            (lambda (lst) (list-update lst 2 string->number)) ;updating amount column to be number
+            (lambda (lst) (list-update lst 1 remove-unnecessary-info))) ;removing unnecessary info from name column
+           line))
+
+;(define (line->transaction line)
+;  (let ([t-info (string-split (remove-doublequote line) ",")])
+;    (
+;  (list-update 
+;   (list-update (string-split (remove-doublequote line) ",")
+;                4
+;                string->number)
+;   2
+;   extract-info))
 
 
-;; accessors to transaction data
+;;; accessors to transaction data
+
+;; basic accessors
 (define (date t) (list-ref t 0))
 
-(define (name t) (list-ref t 2))
+(define (name t) (list-ref t 1))
 
-(define (memo t) (list-ref t 3))
+(define (amount t) (list-ref t 2))
 
-(define (amount t) (list-ref t 4))
+;; more accessors
+(define (month t)
+  (string->number
+   (list-ref (string-split (date t) "/") 0)))
+
+(define (names th)
+  (map (lambda (t) (name t)) th))
+
 
 
 ;; use transaction history to build more specific lists
-(define (spending th)
+(define (spending-th th)
   (filter (lambda (t) (< (amount t) 0)) th))
 
-(define (income th)
+(define (total-spending th)
+  (apply + (map (lambda (t) (amount t)) (spending-th th))))
+
+(define (income-th th)
   (filter (lambda (t) (> (amount t) 0)) th))
 
+(define (total-income th)
+  (apply + (map (lambda (t) (amount t)) (income-th th))))
+
+(define (monthly-th th m)
+  (filter (lambda (t) (= (month t) m)) th))
+
+(define (cat? t-name cat-constant)
+  (ormap (lambda (keyword)
+           (string-contains? t-name keyword))
+         cat-constant))
+
+;; in need of constant modification
+(define (category t-name)
+  (cond [(cat? t-name FOOD-DRINK) "FOOD-DRINK"]
+        [(cat? t-name LEISURE) "LEISURE"]
+        [(cat? t-name TRANSPORTATION) "TRANSPORTATION"]
+        [(cat? t-name ACADEMICS) "ACADEMICS"]
+        [else "uncategorized"]))
+
+(define (categorize th)
+  (map (lambda (t)
+         (list-update t 2
+                      (lambda (t-name) (cons (category t-name) t-name))))
+       th))
+  
+(define (monthly-report th m)
+  (let ([m-th (monthly-th th m)])
+    (printf "Monthly report for ~a~n" m)
+    (printf "Total spending: ~a~n" (total-spending m-th))
+    (printf "Total income: ~a~n" (total-income m-th))))
 
 (define th (process-transaction-history in))
 th
-(spending th)
-(income th)
+;(spending-th th)
+;(income-th th)
+;(total-spending th)
+;(monthly-th th 12)
+;(monthly-report th 12)
+;(remove-duplicates (names th))
+;(categorize th)
+
+;; "TARGET" could be living or food-drink
+;; possible categories:
+;; ACADEMICS (UDEMY)
+;; transportation
+;; acad
+
+        
+        
+        
 
 ;TO-DO
 ;1. Categorize each transaction into food, clothing, etc.
